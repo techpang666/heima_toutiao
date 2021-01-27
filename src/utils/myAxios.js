8e8de4d507a8cd94974c9f066b444e14bd82158a
TECHPANG_dev <TECHPANG_dev@test.com>
2021-01-28 05:41:11 +0800

    test_server

--- src/utils/myAxios.js
+++ src/utils/myAxios.js
@@ -2,8 +2,8 @@
 import axios from 'axios'
 
 // 配置基准路径
-axios.defaults.baseURL = 'http://127.0.0.1:3000' // 测试服务器
-// axios.defaults.baseURL = 'http://157.122.54.189:9083' // 上线服务器
+// axios.defaults.baseURL = 'http://127.0.0.1:3000' // 测试服务器
+axios.defaults.baseURL = 'http://157.122.54.189:9083' // 上线服务器
 
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
